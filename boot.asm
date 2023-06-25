8e322d00f0992f7595a5e28bcd893611599de729
Jkm036 <joshkmuthii@gmail.com>
2023-06-24 21:19:09 -0400

    reading memeory from an emulated herdrive

--- boot.asm
+++ boot.asm
@@ -1,10 +1,41 @@
-ORG 0x7c00
+ORG 0
+;; loading adress for operating systems 
 BITS 16
+_start:
+    jmp short start
+    nop
+times 33 db 0 ; certain computers require this of bootloaders
 start:
-    mov si, message
+    jmp 0x7c0:start_program ; ensure code segment is set to 0x7c0 segment:offset
+start_program:
+    cli ;clear interrupts
+    mov ax, 0x7c0  ; setting segments 
+    mov ds, ax     ; data segment
+    mov es, ax     ; extra segment
+    mov ax, 0x00 
+    mov ss, ax     ; stack segment
+    mov sp, 0x7c00 ; stack offset
+    sti            ; enables interrupts
+    ;----------------------------------------------------------------------
+
+
+    mov ah, 2       ; read sector command
+    mov al, 1       ; reading one sctor
+    mov ch, 0       ; cylinder low 8 bits
+    mov cl, 2       ; read cylinder 2 (1-indexed)
+    mov dh, 0       ; Head number
+    mov bx, buffer  ; must be where we write the memory to
+    int 0x13        ; interupt for reading form a disk
+    jc error
+    
+    mov si, buffer
     call print
     jmp $
-
+    
+error:
+mov si, error_message
+call print
+jmp$
 
 print:
 mov bx, 0
@@ -18,9 +49,11 @@ jmp .loop
 ret
 print_char: 
     mov ah, 0eh
-
     int 0x10
     ret
-message: db 'Hello World!',0
+
+error_message: db 'Failed'
 times 510-($-$$) db 0
 dw 0xAA55
+
+buffer:
